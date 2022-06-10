03212a2064f5b1723f2c9615031a111b12ef4384
ddld93 <umar.jere@gmail.com>
2022-06-10 08:09:10 +0100

    vv

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const path = require('path');
 
 const app = express()
-const port = 7000
+const port = 5000
 app.use(cors());
 
 app.use(express.static(path.join(__dirname, 'build')));
